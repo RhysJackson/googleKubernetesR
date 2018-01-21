c81ed410139e076698ca9bc973e9c84e1b6455e2
Rhys Jackson <rhysjacksonbass@hotmail.co.uk>
2018-01-21 14:59:17 +0000

    Enable use of json client ID credentials
    Changed k8s_auth() to allow for arg 'json'. Can now provide OAuth Client ID credentials via json file.

--- R/k8s_auth.R
+++ R/k8s_auth.R
@@ -2,11 +2,17 @@
 #'
 #' Calls googleAuthR to authenticate with the Google Cloud Platform
 #' @keywords k8s_auth
+#' @param json Path to your json client_secret credentials.
 #' @importFrom googleAuthR gar_auth
 #' @export
 #' @examples
 #' k8s_auth()
+#' k8s_auth(json = "client_secret.json")
 
-k8s_auth <- function() {
-  googleAuthR::gar_auth()
+k8s_auth <- function(json = NULL) {
+  if(is.null(json)) {
+    googleAuthR::gar_auth()
+  } else {
+    googleAuthR::gar_set_client(json)
+  }
 }
